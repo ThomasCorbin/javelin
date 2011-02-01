6e18f0cf3525f86fd6df8df1c5e4608178596585
Thomas Corbin <thomas.corbin@gmail.com>
2011-02-01 16:39:11 -0600

    trying the logging of sql to the console.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,9 @@ SampleApp::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+
+  if "irb" == $0
+    ActiveRecord::Base.logger = Logger.new(STDOUT)
+  end
 end
 
