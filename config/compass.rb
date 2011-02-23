7df067b8314234eec6c86e7b2dd0037036d04877
Thomas Corbin <thomas.corbin@gmail.com>
2011-02-23 09:12:33 -0600

    moved back to compiled, trying to see if we can turn off compilation of scss by rails.

--- config/compass.rb
+++ config/compass.rb
@@ -7,8 +7,10 @@ project_path = Compass::AppIntegration::Rails.root
 # Set this to the root of your project when deployed:
 http_path = "/"
 #css_dir = "public/stylesheets/compiled"
-css_dir = "public/stylesheets"
+css_dir = "public/stylesheets/compiled"
 sass_dir = "app/stylesheets"
 environment = Compass::AppIntegration::Rails.env
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
+
+Sass::Plugin.options[:never_update] = true
