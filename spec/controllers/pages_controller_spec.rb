f4bbb2283d99545ac0ea92eae1c45d881ecb8c6e
Thomas Corbin <thomas.corbin@gmail.com>
2011-03-02 11:32:05 -0600

    added some tests for ch 11 problems

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -17,6 +17,54 @@ describe PagesController do
                         :content => "#{title 'Home'}")
 #                        :content => "Ruby on Rails Tutorial Sample App | Home")
     end
+
+    describe "microposts" do
+
+      before(:each) do
+        @user   = test_sign_in(Factory(:user))
+        @admin  = Factory(:admin)
+
+        (0..50).each do |i|
+          Factory(:micropost, :user => @user, :content => "#{i}")
+        end
+
+        @other_user_micropost = Factory(:micropost, :user => @admin, :content => "other user micropost")
+        @user.microposts << @other_user_micropost
+      end
+
+      it "should have an element for each micropost" do
+        get 'home'
+        @user.microposts[0..2].each do |micropost|
+          response.should have_selector("span", :content => micropost.content)
+        end
+      end
+
+      it "should paginate microposts" do
+        get 'home'
+        response.should have_selector("div.flickr_pagination")
+        response.should have_selector("span.disabled", :content => "Previous")
+        response.should have_selector("a", :href => "/?page=2",
+                                           :content => "2")
+        response.should have_selector("a", :href => "/?page=2",
+                                           :content => "Next")
+      end
+
+      it "should NOT show the delete link posts NOT from the current user" do
+        get 'home'
+
+        response.should_not have_selector("a", :href    => "/microposts/#{@user.microposts.first.id}",
+                                           :rel     => "nofollow",
+                                           :content => "Delete")
+      end
+
+      it "should show the delete link for posts from the current user" do
+        get 'home'
+
+        response.should have_selector("a", :href    => "/microposts/#{@user.microposts[2].id}",
+                                           :rel     => "nofollow",
+                                           :content => "Delete")
+      end
+    end
   end
 
 
